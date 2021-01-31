6b8b07c9c91ddf62e37e7d455efe1204f807fcfe
IzLeandro <34462612+IzLeandro@users.noreply.github.com>
2021-01-31 17:21:05 -0600

    reporte1 Añadido

---
+++ reportes.py
@@ -0,0 +1,17 @@
+#Importaciones
+import csv
+#Funciones
+def reporte1(lista):
+    '''
+    Funcionamiento: crea la base de datos en Excel
+    Entradas: dicccionario de primeros ingresos y matriz de mentores
+    Salidas: nombre del archico .csv
+    '''
+    nombreArchivo="ReporteContactosDelSistema"+".csv"
+    with open(str(nombreArchivo)+".csv","w",newline="") as csvfile:
+        nombreDeCampos=["Nombre","Apellidos","Tipo","Numero","Correo"]
+        escribir= csv.DictWriter(csvfile,fieldnames=nombreDeCampos)
+        escribir.writeheader()
+        for i in lista:
+            escribir.writerow({"Nombre":i.nombre,"Apellidos":i.apellidos,"Tipo":i.tipo,"Numero":i.numero,"Correos":i.correos})
+        return nombreArchivo
